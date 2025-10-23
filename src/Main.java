df6784b56c73c3cd7fdf8a344a2b47995ad4c5e3
Alba Montoro <amonrui0407@g.educaand.es>
2025-10-23 11:54:28 +0200

    Modificacion

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,6 @@
 void main() {
     //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
     // to see how IntelliJ IDEA suggests fixing it.
-    System.out.println("¡Hola y bienvenidos!");
+    System.out.println("¡Hola y bienvenidos! Soy Alba :)");
 
 }
