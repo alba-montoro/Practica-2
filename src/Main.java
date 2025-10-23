06670888f65b034ff1415a13cf5bda745dc6fe96
alba-montoro <amonrui0407@g.educaand.es>
2025-10-23 11:49:24 +0200

    Segundo commit traduccion al castellano

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,6 @@
 void main() {
     //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
     // to see how IntelliJ IDEA suggests fixing it.
-    System.out.println("Hello and welcome!");
+    System.out.println("¡Hola y bienvenidos!");
 
 }
